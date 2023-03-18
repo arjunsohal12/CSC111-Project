f5ad0ac14a811403e1bfa216bb20aec3ff35a1ff
arjunsohal12 <38443000+arjunsohal12@users.noreply.github.com>
2023-03-18 15:50:08 -0400

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -2,11 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import Request, urlopen
 
-website = 'https://en.wikipedia.org/wiki/Canada'
+website = 'https://en.wikipedia.org/wiki/Germany'
 last_page = False
 
 
-def get_url(url: str) -> dict:
+def get_url(url: str) -> dict | None:
     request = requests.get(url)
     if request.status_code != 200:
         print('Error, request status code of {}'.format(request.status_code))
@@ -39,8 +39,12 @@ def getlinks(soup: BeautifulSoup) -> dict:
 
 
 def getTopOccurences(items: dict) -> dict:
-     sorted_dict = sorted(items.items(), key=lambda x: x[1], reverse=True)
-     if len(sorted_dict) > 10:
-         sorted_dict = sorted_dict[:10]
-     converted_dict = dict(sorted_dict)
-     return converted_dict
+    sorted_dict = sorted(items.items(), key=lambda x: x[1], reverse=True)
+    if len(sorted_dict) > 10:
+        sorted_dict = sorted_dict[:10]
+    converted_dict = dict(sorted_dict)
+    return converted_dict
+
+
+if __name__ == "__main__":
+    print(get_url(website))
