39fcdc3ae60e62ec0d65df1a98b9a213031f44cc
Mani Tahami <85440257+mani1381@users.noreply.github.com>
2023-03-29 14:26:06 -0400

    Update 2DGraphGenerator.py

--- 2DGraphGenerator.py
+++ 2DGraphGenerator.py
@@ -3,15 +3,12 @@ import random
 
 import pygame
 
-from Project.GraphHelper import Graph
-from graph_data import graph
-from math import dist
-import GraphHelper
+from Project.GraphHelper import Graph, generate_graph
 
 # constants
-display_width = 800
-display_height = 600
-radius = 30
+display_width = 1000
+display_height = 800
+radius = 15
 
 # colors
 white = (255, 255, 255)  # discovered state
@@ -21,7 +18,6 @@ blue = (50, 50, 160)  # completed state fill
 def run(dict_so_far: dict[str:tuple[int, int]], final_graph: Graph):
     global screen, edges  # to share with other methods
 
-    build_edges()
     pygame.init()
 
     screen = pygame.display.set_mode((display_width, display_height))
@@ -32,13 +28,13 @@ def run(dict_so_far: dict[str:tuple[int, int]], final_graph: Graph):
     # for n1, n2 in edges:
     #     pygame.draw.line(screen, white, graph[n1][0], graph[n2][0], 2)
 
-    for node in final_graph.get_vertices():
-        for node_2 in final_graph.get_vertices():
-            if final_graph.get_vertices()[node_2] in final_graph.get_vertices()[node].get_neighbours():
-                pygame.draw.line(screen, white, dict_so_far[node], dict_so_far[node_2], 2)
+    neighbours_list = all_neighbours(final_graph)
+
+    for neighbours_pair in neighbours_list:
+        pygame.draw.line(screen, white, dict_so_far[neighbours_pair[0]], dict_so_far[neighbours_pair[1]], 2)
 
     for node in dict_so_far:
-        circle_fill(dict_so_far[node], white, blue, 25, 2)
+        circle_fill(dict_so_far[node], white, blue, radius, 2)
 
     pygame.display.update()
 
@@ -53,32 +49,40 @@ def circle_fill(xy, line_color, fill_color, radius, thickness):
     pygame.draw.circle(screen, fill_color, xy, radius - thickness)
 
 
-def edge_id(n1, n2):  # normalize id for either order
-    # (1,2) and (2,1) become (1,2)
-    return tuple(sorted((n1, n2)))
-
-
-def build_edges():
-    global edges
-    edges = {}
-    for n1, (_, adjacents) in enumerate(graph):
-        for n2 in adjacents:
-            eid = edge_id(n1, n2)
-            if eid not in edges:
-                edges[eid] = (n1, n2)
-
-
 def createCoodinates(graph: Graph) -> dict[str:tuple[int, int]]:
     dict_so_far = {}
     for node in graph.get_vertices():
-        dict_so_far[node] = generate_random_coordiante(dict_so_far)
+        new_coordinates = generate_random_coordiante(dict_so_far)
+        dict_so_far[node] = new_coordinates
     return dict_so_far
 
 
 def generate_random_coordiante(dict_so_far: dict[str:tuple[int, int]]) -> tuple[int, int]:
-    random_tuple = (round(random.uniform(0, display_height)), round(random.uniform(0, display_width)))
-    for node in dict_so_far:
-        if math.dist(random_tuple, dict_so_far[node]) < radius:
-            random_tuple = (round(random.uniform(0, display_height)), round(random.uniform(0, display_width)))
+    random_tuple = (
+        round(random.uniform(radius, display_width - radius)), round(random.uniform(radius, display_height - radius)))
+    i = 0
+    while any(math.dist(random_tuple, dict_so_far[node]) < 3 * radius for node in dict_so_far):
+        print(i)
+        i += 1
+
+        random_tuple = (round(random.uniform(radius, display_width - radius)),
+                        round(random.uniform(radius, display_height - radius)))
 
     return random_tuple
+
+
+def all_neighbours(graph: Graph) -> list[tuple[str, str]]:
+    list_so_far = []
+    for node in graph.get_vertices():
+        for neighbour in graph.get_vertices()[node].get_neighbours():
+            if tuple(sorted((node, neighbour.get_item()))) not in list_so_far:
+                list_so_far.append(tuple(sorted((node, neighbour.get_item()))))
+    return list_so_far
+
+
+graph1 = Graph()
+graph1.add_vertex('https://en.wikipedia.org/wiki/Canada')
+generate_graph(graph1, 'https://en.wikipedia.org/wiki/Canada', 2)
+dicti = createCoodinates(graph1)
+run(dicti, graph1)
+
